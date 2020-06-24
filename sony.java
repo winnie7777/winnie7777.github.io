86e978ec731e3dc4463969a44f3b265d262caf1f
winnie7777 <66389098+winnie7777@users.noreply.github.com>
2020-06-24 12:42:19 -0700

    final

---
+++ sony.java
@@ -0,0 +1,11 @@
+var topnav = document.getElementById('topnav')</script>
+  window.oncroll = function()
+    if(windows.pageYOffset > 100){
+
+      nav.style.position= "fixed";
+      nav.style.top = 0;
+    }
+    else{
+      box.style.position = "absolute";
+      box.style.top =100'
+    }
